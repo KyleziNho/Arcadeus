0143693ad4ca321602a4cc878694517d7d73e006
Your Name <kos33@bath.ac.uk>
2025-06-01 11:31:22 +0100

    Excel add-in static files for hosting

---
+++ taskpane/taskpane.d.ts
@@ -0,0 +1,47 @@
+interface DealAssumptions {
+    dealName: string;
+    dealSize: number;
+    ltv: number;
+    holdingPeriod: number;
+    revenueGrowth: number;
+    exitMultiple: number;
+    selectedRange?: string;
+    rangeData?: any[][];
+}
+interface ChatMessage {
+    role: 'user' | 'assistant';
+    content: string;
+}
+declare class MAModelingAddin {
+    private chatMessages;
+    private selectedRange;
+    private openaiApiKey;
+    constructor();
+    private initializeAddin;
+    private promptForApiKey;
+    private saveApiKey;
+    private selectAssumptionsRange;
+    private parseAssumptionsFromRange;
+    private updateFormWithAssumptions;
+    private collectAssumptions;
+    private generateModel;
+    private createModelSheets;
+    private populateAssumptionsSheet;
+    private populateNPVSheet;
+    private populatePLSheet;
+    private populateCFSheet;
+    private calculateMetrics;
+    private generateCashFlows;
+    private calculateWithOpenAI;
+    private calculateMetricsFallback;
+    private validateModel;
+    private sendChatMessage;
+    private processWithAI;
+    private getExcelContext;
+    private offerToImplementFormula;
+    private implementSuggestedFormula;
+    private addChatMessage;
+    private showLoading;
+    private showStatus;
+    private getColumnLetter;
+}
